44bfb8dac7d87c9d9566464f0b33b1fe241d493c
CedPoilly <cedpoilly@gmail.com>
2019-06-25 21:21:25 +0400

    Push demo

--- www/build/p-dbmafcaa.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as r}from"./p-8f39645a.js";class e{constructor(r){t(this,r)}getText(){return(this.first||"")+((t=this.middle)?` ${t}`:"")+((r=this.last)?` ${r}`:"");var t,r}render(){return r("div",null,"Hello, World! I'm ",this.getText())}static get style(){return""}}export{e as my_component};
